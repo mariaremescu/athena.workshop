4be55ff9bef2b24fae52626ea2291c85e5984143
remescumaria <mariaremescu@gmail.com>
2025-10-22 12:59:19 +0300

    feat: put emojis, give the option to replay fix: put the slash bar the other way

--- hangman.py
+++ hangman.py
@@ -15,49 +15,60 @@ difficulty_settings = {
 def play_hangman():
 
     word = random.choice(word_list)
-    print(word)
     word_display = ['_' for letters in word]
     guessed_letters = []
     correct_letters = []
+
     for letters in word:
         correct_letters.append(letters)
-    print("Choose the difficulty: ")
+    print("Choose the difficulty: easy / medium / hard")
 
     while True:
-       
-        difficulty = input("difficulty: ").lower()
-        print(difficulty)
+        difficulty = input("Choose difficulty: ").lower()
         if difficulty in difficulty_settings.keys():
             break
-        print("Invalid choice, try again")
+        print("\nInvalid choice, try again")
 
-    
-    
     lives = difficulty_settings[difficulty]
-    print("\n The game starts 🎮!!")
-
-    while lives > 0:
+    print("\n!! The game starts 🎮!!")
+    variable=False
+    while lives > 0 and variable==False:
         for i in word_display:
-            print(i,end=" ")
-        print(f"You have {lives}/n")
+            print(i, end=" ")
+        print(f"\nYou have {lives} lives\n")
         human_choice = input("Choose a letter : \n").lower()
-        
+
         while 'a' > human_choice and human_choice > 'z':
-            print("/nThe character you entered is invalid, try again")
-            human_choice = input("/nChoose a letter : ").lower()
+            print("\nThe character you entered is invalid, try again")
+            human_choice = input("\nChoose a letter : ").lower()
 
         if human_choice in correct_letters:
-            
             guessed_letters.append(human_choice)
-
             for index in range(len(word)):
-                if word[index]==human_choice:
+                if word[index] == human_choice:
                     word_display[index] = human_choice
-
+                    variable = True
+                    for i in range(len(word_display)):
+                            if '_' in word_display[i]:
+                                variable = False
+                    if variable == True:
+                        print("You won!!🎉🎉✨")
+                        break
 
         else:
             lives = lives - 1
             guessed_letters.append(human_choice)
         print(f" Letters guessed {guessed_letters}")
+    
+   
+    if variable == False:
+        print("You lost😞😞") 
+        choice = input("Do you want to play again😊?? Y/N ").lower()
+        if choice == 'n':
+            print("💔")
+        elif choice == 'y':
+            play_hangman()
 
-play_hangman()           
+
+
+play_hangman()
